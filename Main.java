b0f3ee9e3561ac55b138b25aec44f70b731e669e
Nishanth <nishanthvangara@gmail.com>
2022-11-24 22:37:44 -0600

    code update

--- Main.java
+++ Main.java
@@ -4,39 +4,39 @@ import java.io.IOException;
 import java.util.Hashtable;
 import java.util.Map;
 
-class Crop{
+class Crop {
     String name; // space
-    int maxValue;
-    int minValue;
+    int maxYieldValue;
+    int minYieldValue;
+    int maxPriceValue;
+    int minPriceValue;
 
-    public Crop(String name, int minValue, int maxValue){
+    public Crop(String name, int minYieldValue, int maxYieldValue, int maxPriceValue, int minPriceValue) {
         this.name = name;
-        this.minValue = minValue;
-        this.maxValue = maxValue;
+        this.minYieldValue = minYieldValue;
+        this.maxYieldValue = maxYieldValue;
+        this.maxPriceValue = maxPriceValue;
+        this.minPriceValue = minPriceValue;
     }
 }
 
-class State{
+class State {
     String name;
-    int minValue;
-    int maxValue;
+    int minYieldValue;
+    int maxYieldValue;
 
-    public State(String name, int minValue, int maxValue){
+    public State(String name, int minYieldValue, int maxYieldValue) {
         this.name = name;
-        this.minValue = minValue;
-        this.maxValue = maxValue;
+        this.minYieldValue = minYieldValue;
+        this.maxYieldValue = maxYieldValue;
     }
 }
 
 public class Main {
     public static void main(String[] args) {
 
-//        Cultivation(StateID,CropID,Year,Yield)  :- StateID=5, CropID =1, Year =2011, Yield=2124.
-//        int states = 13;
-//        int crops = 14;
-
-        int startYear= 2011;
-        int endYear= 2021;
+        int startYear = 2011;
+        int endYear = 2021;
 
         Hashtable<Integer, State> states = GenerateStatesObject();
         Hashtable<Integer, Crop> crops = GenerateCropRange();
@@ -44,21 +44,24 @@ public class Main {
         StringBuilder finalContent = new StringBuilder();
         finalContent.append("begin %MLPQ% \n");
 
-        for (; startYear<=endYear; startYear++){
+        for (; startYear <= endYear; startYear++) {
 
-            for(Map.Entry<Integer, State> state : states.entrySet()) {
+            for (Map.Entry<Integer, State> state : states.entrySet()) {
                 int stateId = state.getKey();
                 State stateValue = state.getValue();
 
-                for(Map.Entry<Integer, Crop> crop : crops.entrySet()) {
+                for (Map.Entry<Integer, Crop> crop : crops.entrySet()) {
                     int cropId = crop.getKey();
                     Crop cropValue = crop.getValue();
 
-                    int  min_avg_StateAndCrop = (stateValue.minValue + cropValue.minValue)/2;
-                    int  max_avg_StateAndCrop = (stateValue.maxValue + cropValue.maxValue)/2;
+                    int min_avg_yield_StateAndCrop = (stateValue.minYieldValue + cropValue.minYieldValue) / 2;
+                    int max_avg_yield_StateAndCrop = (stateValue.maxYieldValue + cropValue.maxYieldValue) / 2;
 
-
-                    finalContent.append( GetLine(stateId, cropId, startYear, getRandomNumber(min_avg_StateAndCrop, max_avg_StateAndCrop)));
+                    finalContent.append(GetLine(stateId,
+                            cropId,
+                            startYear,
+                            getRandomNumber(min_avg_yield_StateAndCrop, max_avg_yield_StateAndCrop),
+                            getRandomNumber(cropValue.minPriceValue, cropValue.maxPriceValue)));
 
                 }
             }
@@ -69,34 +72,34 @@ public class Main {
         WriteToFile(finalContent.toString());
     }
 
-    private static Hashtable<Integer, Crop> GenerateCropRange(){
+    private static Hashtable<Integer, Crop> GenerateCropRange() {
         Hashtable<Integer, Crop> CropIdAndName = new Hashtable<>();
 
-        CropIdAndName.put(1, new Crop("Paddy",1500, 3000));
-        CropIdAndName.put(2, new Crop("Wheat",2000, 5000));
-        CropIdAndName.put(3, new Crop("Jowar",500, 2500));
-        CropIdAndName.put(4, new Crop("Bajra",900, 2500));
-        CropIdAndName.put(5, new Crop("Maize",1500, 7000));
-        CropIdAndName.put(6, new Crop("Ragi",500, 2500));
-        CropIdAndName.put(7, new Crop("Barley",1000,2500));
-        CropIdAndName.put(8, new Crop("Sugarcane",700, 3500));
-        CropIdAndName.put(9, new Crop("Groundnut", 800, 2000));
-        CropIdAndName.put(10, new Crop("Sesamum",900, 1500));
-        CropIdAndName.put(11, new Crop("Castor",1400, 4500));
-        CropIdAndName.put(12, new Crop("Linseed",3000, 6000));
-        CropIdAndName.put(13, new Crop("Cotton",1500, 2500));
-        CropIdAndName.put(14, new Crop("Jute",3000,4000));
+        CropIdAndName.put(1, new Crop("Paddy", 1500, 3000, 150, 170));
+        CropIdAndName.put(2, new Crop("Wheat", 2000, 5000, 155, 165));
+        CropIdAndName.put(3, new Crop("Jowar", 500, 2500, 150, 163));
+        CropIdAndName.put(4, new Crop("Bajra", 900, 2500, 170, 195));
+        CropIdAndName.put(5, new Crop("Maize", 1500, 7000, 145, 175));
+        CropIdAndName.put(6, new Crop("Ragi", 500, 2500, 210, 230));
+        CropIdAndName.put(7, new Crop("Barley", 1000, 2500, 150, 175));
+        CropIdAndName.put(8, new Crop("Sugarcane", 700, 3500, 100, 125));
+        CropIdAndName.put(9, new Crop("Groundnut", 800, 2000, 120, 170));
+        CropIdAndName.put(10, new Crop("Sesamum", 900, 1500, 160, 200));
+        CropIdAndName.put(11, new Crop("Castor", 1400, 4500, 90, 130));
+        CropIdAndName.put(12, new Crop("Linseed", 3000, 6000, 140, 175));
+        CropIdAndName.put(13, new Crop("Cotton", 1500, 2500, 95, 125));
+        CropIdAndName.put(14, new Crop("Jute", 3000, 4000, 180, 230));
 
         return CropIdAndName;
     }
 
-    private static Hashtable<Integer, State>  GenerateStatesObject() {
+    private static Hashtable<Integer, State> GenerateStatesObject() {
         Hashtable<Integer, State> stateIdAndName = new Hashtable<>();
         //Paddy range
-        stateIdAndName.put(1, new State("Uttar Pradesh", 2000,  2500));
-        stateIdAndName.put(2, new State("Madhya Pradesh", 1600, 2200 ));
-        stateIdAndName.put(3, new State("Punjab",3700, 4500 ));
-        stateIdAndName.put(4, new State("Rajasthan", 500, 1500 ));
+        stateIdAndName.put(1, new State("Uttar Pradesh", 2000, 2500));
+        stateIdAndName.put(2, new State("Madhya Pradesh", 1600, 2200));
+        stateIdAndName.put(3, new State("Punjab", 3700, 4500));
+        stateIdAndName.put(4, new State("Rajasthan", 500, 1500));
         stateIdAndName.put(5, new State("West Bengal", 2500, 3200));
         stateIdAndName.put(6, new State("Haryana", 3000, 3500));
         stateIdAndName.put(7, new State("Bihar", 2200, 2600));
@@ -107,18 +110,18 @@ public class Main {
         stateIdAndName.put(12, new State("Karnataka", 2400, 3400));
         stateIdAndName.put(13, new State("Others", 2500, 3500));
 
-        return  stateIdAndName;
+        return stateIdAndName;
     }
 
     static int getRandomNumber(int min, int max) {
         return (int) ((Math.random() * (max - min)) + min);
     }
 
-    static String GetLine(int stateId, int cropId, int year, int yield){
-        return String.format("Cultivation(StateID,CropID,Year,Yield)  :- StateID=%s, CropID=%s, Year=%s, Yield=%s. \n", stateId, cropId, year, yield);
+    static String GetLine(int stateId, int cropId, int year, int yield, int price) {
+        return String.format("Cultivation(StateID,CropID,Year,Yield)  :- StateID=%s, CropID=%s, Year=%s, Yield=%s, Price=%s. \n", stateId, cropId, year, yield, price);
     }
 
-    static void WriteToFile(String output){
+    static void WriteToFile(String output) {
         try {
             File myObj = new File("output.txt");
             if (myObj.createNewFile()) {
